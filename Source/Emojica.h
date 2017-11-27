e87ac9f23d18529f21d678164546912833d3dd5b
Dan Lindholm <main@xoudini.com>
2017-11-27 23:59:53 +0200

    Bump year in headers.

--- Source/Emojica.h
+++ Source/Emojica.h
@@ -1,7 +1,7 @@
 //
 //  ------------------------------------------------------------------------
 //
-//  Copyright 2016 Dan Lindholm
+//  Copyright 2017 Dan Lindholm
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
